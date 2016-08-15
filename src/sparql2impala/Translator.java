1646e7091ad1a4aeee881909fd738b673ff70449
raphaelapn <raphaela.pn@gmail.com>
2016-08-15 03:54:12 -0300

    Update Translator.java

--- src/sparql2impala/Translator.java
+++ src/sparql2impala/Translator.java
@@ -25,14 +25,7 @@ import com.hp.hpl.jena.sparql.algebra.optimize.TransformJoinStrategy;
 import com.hp.hpl.jena.sparql.core.QueryEngineFactoryWrapper;
 
 
-/**
- * Main Class of the ImpalaSPARQL translator.
- * This class generates the Algebra Tree for a SPARQL query,
- * performs some optimizations, translates the Algebra tree into an
- * ImpalaOp tree, initializes the translation and collects the final output.
- *
- * @author Antony Neu
- */
+
 public class Translator  {
 
     String inputFile;
@@ -40,78 +33,50 @@ public class Translator  {
     private String sqlScript;
     private ImpalaOp impalaOpRoot;
     
-    // Define a static logger variable so that it references the corresponding Logger instance
+
     private static final Logger logger = Logger.getLogger(Translator.class);
 
 
-    /**
-     * Constructor of the ImpalaSPARQL translator.
-     *
-     * @param _inputFile SPARQL query to be translated
-     * @param _outputFile Output script of the translator
-     */
+
     public Translator(String _inputFile, String _outputFile) {
     	sqlScript = "";
         inputFile = _inputFile;
         outputFile = _outputFile;
     }
 
-    /**
-     * Sets the delimiter of the RDF Triples.
-     * @param _delimiter
-     */
+
+
     public void setDelimiter(String _delimiter) {
         Tags.delimiter = _delimiter;
     }
 
-    /**
-     * Sets if pefixes in the RDF Triples should be expanded or not.
-     * @param value
-     */
+
     public void setExpandMode(boolean value) {
         Tags.expandPrefixes = value;
     }
 
-    /**
-     * Enables or disables optimizations of the SPARQL Algebra.
-     * Optimizer is enabled by default.
-     * @param value
-     */
+
     public void setOptimizer(boolean value) {
         Tags.optimizer = value;
     }
 
-    /**
-     * Enables or disables Join optimizations.
-     * Join optimizations are disabled by default
-     * @param value
-     */
+
     public void setJoinOptimizer(boolean value) {
         Tags.joinOptimizer = value;
     }
 
-    /**
-     * Enables or disables Filter optimizations.
-     * Filter optimizations are enabled by default.
-     * @param value
-     */
+
     public void setFilterOptimizer(boolean value) {
         Tags.filterOptimizer = value;
     }
 
-    /**
-     * Enables or disables BGP optimizations.
-     * BGP optimizations are enabled by default.
-     * @param value
-     */
+
     public void setBGPOptimizer(boolean value) {
         Tags.bgpOptimizer = value;
     }
 
 
-    /**
-     * Translates the SPARQL query into a sql script for use with Impala.
-     */
+
     public void translateQuery() {
         //Parse input query
         Query query = QueryFactory.read("file:"+inputFile);
